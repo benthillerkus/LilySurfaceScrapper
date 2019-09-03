7c6d171afe367dee4ce1cf75ada1b7e94d343d00
ogierm <horstikugel@gmail.com>
2019-09-03 15:12:01 +0200

    Support for normal, height and glossiness maps.
    Unfortunately only working for single sided textures right now 🐞

--- blender/LilySurfaceScrapper/CyclesMaterialData.py
+++ blender/LilySurfaceScrapper/CyclesMaterialData.py
@@ -13,12 +13,12 @@ class CyclesMaterialData(MaterialData):
     # Translate our internal map names into cycles principled inputs
     input_tr = {
         'baseColor': 'Base Color',
-        'normal': 'Normal',
         'roughness': 'Roughness',
         'metallic': 'Metallic',
         'specular': 'Specular',
         'opacity': 'Alpha',
         'emission': 'Emission',
+        'normal': '',
         'height': '',
         'ambientOcclusion': '', # https://github.com/KhronosGroup/glTF-Blender-IO/issues/123
         'glossiness': '',
@@ -64,23 +64,41 @@ class CyclesMaterialData(MaterialData):
                 mat.blend_method = 'BLEND'
 
         front = back if not front else front # In case just the backside texture was chosen
-        
-        # Wire up all of the nodes traditionally
+
+        def advanced_setup(name, node):
+            if name == "glossiness":
+                invert_node = nodes.new(type="ShaderNodeInvert")
+                links.new(node.outputs["Color"], invert_node.inputs["Color"])
+                links.new(invert_node.outputs["Color"], principled.inputs["Roughness"])
+            elif name == "height":
+                displacement_node = nodes.new(type="ShaderNodeDisplacement")
+                links.new(node.outputs["Color"], displacement_node.inputs["Height"])
+                links.new(displacement_node.outputs["Displacement"], mat_output.inputs[2])
+            elif name == "normal":
+                normal_node = nodes.new(type="ShaderNodeNormalMap")
+                links.new(node.outputs["Color"], normal_node.inputs["Color"])
+                links.new(normal_node.outputs["Normal"], principled.inputs["Normal"])
+
         if not back: # If there is no item in the back dictionary
             for name, node in front.items():
                 if __class__.input_tr.get(name):
                     links.new(node.outputs["Color"], principled.inputs[__class__.input_tr[name]])
+                else:
+                    advanced_setup(name, node)
         else:
             geometry_node = nodes.new("ShaderNodeNewGeometry")
             def setup(name, front, back, mix):
                 links.new(geometry_node.outputs["Backfacing"], mix.inputs[0])
                 links.new(front.outputs["Color"], mix.inputs[1])
                 links.new(back.outputs["Color"], mix.inputs[2])
-                links.new(mix.outputs["Color"], principled.inputs[__class__.input_tr[name]])
+                if __class__.input_tr.get(name):
+                    links.new(mix.outputs["Color"], principled.inputs[__class__.input_tr[name]])
+                else:
+                    advanced_setup(name, mix)
             for name, node in front.items():
-                if back.get(name) and __class__.input_tr.get(name):
+                if back.get(name):
                     setup(name, node, back[name], nodes.new(type="ShaderNodeMixRGB"))
-
+        
         autoAlignNodes(mat_output)
 
         return mat
